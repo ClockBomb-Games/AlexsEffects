a7d04691ececcf5375ba495595ca6e372b15d1e7
RobotoSkunk <contact@robotoskunk.com>
2024-07-12 14:14:00 -0600

    First commit

---
+++ src/client/java/com/robotoskunk/skunks_tweaks/SkunksTweaksClient.java
@@ -0,0 +1,10 @@
+package com.robotoskunk.skunks_tweaks;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SkunksTweaksClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
